facf486cad6b669076521f5af3aab42fdb038654
LueDev <41023873+LueDev@users.noreply.github.com>
2023-11-04 00:50:01 -0400

    Update app.js
    Messy right now. Learned about turf.js, a geospatial analysis js library that can calculate routes given the curvature of the earth for advanced geospatial queries and analysis. So far, incorporated the bbox method from turf to calculate the bounding box for each zone selected and storing the results in the selectedZoneBBOX obj. Figured this is smarter since the key is the zone but this should all be compiled into a single object and updated non-destructively going forward. For now, we have the zone coords and when calling the google places api, I can use a forEach on the bbox obj and store the results in google places api in an object to iterate over along with filtering for only the places that are within the selected with turfs booleanpointinpolygon method. More on that in the next git commit

--- app.js
+++ app.js
@@ -1,64 +1,13 @@
-/*
-GOAL: Create a map based application similar to this: https://boundaries.beta.nyc/?map=bid
-that can be integrated into other applications. The map has functions that returns values based on the data being looked for. 
+/**
+ * This script handles the initialization and interaction with a Google Map instance.
+ * The map integrates multiple GeoJSON layers and provides functionality for selecting
+ * zones, fetching places within those zones, and applying filters to the displayed places.
+ */
 
-For example, If I select certain sections in the map and looking for restaurants, I can interact with the map and an API response 
-can be returned where the borough, sub-borough, 
-
-Long Term Goal: Develop a developer friendly mapping engine similar to ArcGIS but that is cheaper or open sourced 
-
-Creating a developer-friendly mapping system that rivals established platforms like ArcGIS is an ambitious endeavor, but it's certainly feasible with the right approach. Here are some suggestions to help you get started:
-
-User-Friendly API:
-
-Design a simple and intuitive RESTful API that allows developers to easily fetch and manipulate geographic data.
-Provide comprehensive documentation with examples, tutorials, and interactive API explorers.
-Ensure your API supports common data formats like GeoJSON and KML for interoperability.
-Modular Design:
-
-Create a modular system where developers can choose which layers (e.g., city council districts, borough boundaries, community districts) they want to include in their applications.
-Offer pre-built modules for common tasks, such as geocoding, routing, and spatial analysis, to simplify development.
-Scalable Infrastructure:
-
-Utilize cloud services for hosting your platform to ensure scalability and reliability. Consider using serverless architectures to optimize for usage-based scaling.
-Implement a content delivery network (CDN) to serve static assets like map tiles quickly and efficiently.
-Customization and Extensibility:
-
-Allow developers to style maps according to their brand guidelines and application themes.
-Provide a plugin system or support for third-party extensions to enable developers to add custom functionality.
-Interactive Map SDK:
-
-Develop a robust front-end SDK that facilitates map interaction, such as zooming, panning, and clicking on layers to display additional information.
-Ensure your SDK is cross-platform and supports various frameworks and languages.
-Community Engagement:
-
-Foster a developer community by providing forums, chat channels, and regular meetups or webinars.
-Encourage contributions through open-source initiatives or developer incentive programs.
-Support and SLAs:
-
-Offer responsive support with multiple channels like email, chat, and phone.
-Provide Service Level Agreements (SLAs) to guarantee uptime and performance for enterprise users.
-Legal and Compliance:
-
-Make sure to have clear terms of service, especially regarding data usage and privacy.
-Stay compliant with regulations like GDPR for handling user data.
-Examples and Use Cases:
-
-Showcase real-world applications and case studies that demonstrate the capabilities and benefits of your platform.
-Provide sample apps and code snippets that developers can use as a starting point.
-Feedback Loop:
-
-Implement a system for collecting and acting on user feedback to continuously improve your platform.
-Regularly update your roadmap and communicate changes and new features to your user base.
-By focusing on simplicity, performance, and a developer-centric approach, you can position your platform as a more accessible alternative to complex systems like ArcGIS. Remember that building a community and a robust ecosystem around your platform can significantly contribute to its success and adoption.
-
-TODO[]: Use the places api to list places on the map with advanced markers (maybe a way to customize marker per filter criteria. Suppose we can filter by more than one thing on a given selected zone (like housing prices and restaurants) to make house hunting much more exciting. 
-TODO[]: Calculate the bounds within a selected zone (the answer could be shapefiles but requires further investigation) and return the coordinates usable when calling the google places api
-TODO[]: Add a mousehover event listener that will show the zoneID and if the filtered data is hidden on the map, give the choice to show the results on the infoWindow. Can I customize the infowindow to include the data the user selects rather than just the data set by the program? much more interesting. For example, if I'm looking at prices of homes and number of ammenities it has. Can that be the data shown in the infoWindow and anything else we'd like to add? The UI could be tricky for that but think it through.
-*/
-
-
-//TODO[x]: Create an array of objects that contain the information for each of the geojson files.
+/**
+ *  Configuration for the various GeoJSON layers available in the application
+ *  CREATING A GEOJSON FILE: https://www.youtube.com/watch?v=QbmKn7xZmR4
+ */
 const layersConfig = [
   {
     id: "boroughBoundaries",
@@ -111,40 +60,115 @@ const layersConfig = [
   },
 ];
 
-//This will hold the selectedZones when clicking on individual zones
+// Object to track the selected zones on the map
 let selectedZones = {};
+let zonesListed = {};
 
-//TODO[x]: Initiate the map, center it at Chelsea, NYC.
-//Load the GeoJSON layers (these can stack but not visually on the map yet.)
-function initMap() {
+// Global map object
+let map;
+
+// Store the GeoJSON features
+let geoJsonFeatures = {}
+let selectedZoneCoords = {}
+let selectedZoneBBOX = {}
+
+/**
+ * Initializes the Google Map instance, centers it at the specified coordinates,
+ * and loads the active GeoJSON layers.
+ * Default map location is Chelsea, NYC.
+ * For Guidance: //https://developers.google.com/maps/documentation/javascript/controls
+ */
+async function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 40.7465, lng: -74.0014 },
     zoom: 13,
+    // disableDefaultUI: true,
   });
 
-  // Load GeoJSON layers. Currently only one can be used at a time. Maybe implement a toggle feature. 
+  // Load GeoJSON layers. Currently only one can be used at a time. Maybe implement a toggle feature.
   layersConfig.forEach((layer) => {
     if (layer.active) {
-      console.log(layer);
+      console.log("LAYER: ", layer);
       loadGeoJsonLayer(layer);
     }
   });
 }
 
+/**
+ * Helper function to extract the zone ID from a GeoJSON feature.
+ * @param {Object} feature - The GeoJSON feature
+ * @returns {string} - The zone ID
+ * to be used with geojson object
+ */
+function getZoneId(feature) {
+  return (
+    feature.getProperty("boro_cd") ||
+    feature.getProperty("boro_code") ||
+    feature.getProperty("cong_dist") ||
+    feature.getProperty("elect_dist") ||
+    feature.getProperty("precinct") ||
+    feature.getProperty("school_dist") 
+  );
+}
 
-//TODO[x]:Load the boundaries of the loaded layer config.
-//FIXME: When two layer configs are active, the map doesn't know which I'm interacting with because they're layered on top of one another. I can only use one layer config at a time for now.
+//to be used with json
+function getZoneFromFeature(feature){
+  return (
+    feature['properties']['boro_cd'] ||
+    feature['properties']['boro_code'] ||
+    feature['properties']['cong_dist'] ||
+    feature['properties']['elect_dist'] ||
+    feature['properties']['precinct'] ||
+    feature['properties']['school_dist']
+  )
+}
+
+// Function to compute the bounding box for each selected zone
+function computeBoundingBoxes() {
+  Object.entries(selectedZoneCoords).forEach(([zoneId, geometry]) => {
+    const bbox = turf.bbox(geometry); 
+    selectedZoneBBOX[zoneId] = bbox
+    console.log(`Bounding box for zone ${zoneId}:`, selectedZoneBBOX[zoneId]);
+    console.log(`All Selected BBOX Coords: `, selectedZoneBBOX)
+  });
+}
+
+
+/**
+ * Loads a GeoJSON layer onto the map and sets up event listeners for interaction.
+ * @param {Object} layerConfig - Configuration object for the layer
+ * 
+ * Data Feature in Google Maps API:
+ * Within the Google Maps API, a "Data.Feature" refers to a specific class used in the Data layer of the API. The Data layer    provides a container for arbitrary geospatial data (points, line strings, polygons, etc.). A "Data.Feature" is an individual element in this layer that can represent a single point, line, or polygon. It can hold geometry, a set of properties, and an ID.
+ * Here's an example of how a Data.Feature might be used in the Google Maps API:
+ * javascript
+ * Copy code
+ * var myFeature = new google.maps.Data.Feature({
+ * geometry: new google.maps.Data.Point(new google.maps.LatLng(37.422, -122.084)),
+ * id: 'myFeature',
+ * properties: {
+ *  name: 'Googleplex',
+ *   category: 'office'
+ * }
+ * });
+ * In this example, myFeature is a Data.Feature with a point geometry representing a location, an ID, and a set of properties including a name and category.
+ */
 function loadGeoJsonLayer(layerConfig) {
   fetch(`http://localhost:3000/geojson/${layerConfig.name}.geojson`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      console.log("Geo JSON Data: ", data);
+ 
+      // Store the GeoJSON features
+      geoJsonFeatures[layerConfig.id] = data.features;
+
+      //Creates a data layer on the maps api. This allows us to visualize and load the data as a set.
       map.data.addGeoJson(data);
 
       // Style and click event for GeoJSON layer
       map.data.setStyle((feature) => {
         const zoneId = feature.getId();
-        const isSelected = !!selectedZones[zoneId];
+        const isSelected = selectedZones[zoneId];     
         return {
           fillColor: isSelected ? "#00FF00" : "#FF3333",
           strokeColor: isSelected ? "#00FF00" : "#FF5555",
@@ -152,66 +176,98 @@ function loadGeoJsonLayer(layerConfig) {
         };
       });
 
-      //Will grab the id of the object passed. Since all geojsons have different ids, this will grab them all.
-      map.data.addListener("click", (event) => {
-        const zoneId =
-          event.feature.getProperty("boro_cd") ||
-          event.feature.getProperty("boro_code") ||
-          event.feature.getProperty("cong_dist") ||
-          event.feature.getProperty("elect_dist") ||
-          event.feature.getProperty("precinct") ||
-          event.feature.getProperty("school_dist");
-        console.log(zoneId);
+        // Will grab the id of the object passed. Since all geojsons have different ids, this will grab them all.
+        map.data.addListener("click", (event) => {
+          const zoneId = getZoneId(event.feature);
+          console.log("ZONE ID: ", zoneId);
 
-        if (selectedZones[zoneId]) {
-          delete selectedZones[zoneId];
-        } else {
-          selectedZones[zoneId] = true;
-        }
+          if (selectedZones[zoneId]) {
+            delete selectedZones[zoneId];
+            delete selectedZoneBBOX[zoneId]
+          } else {
+            selectedZones[zoneId] = true;
+
+            console.log(geoJsonFeatures)
+            // Retrieve geometry data for the selected zone
+            const geometry = geoJsonFeatures[layerConfig.id].find(
+              (feature) => 
+              getZoneFromFeature(feature) === zoneId
+            )?.geometry;
+            selectedZoneCoords[zoneId] = geometry
+            computeBoundingBoxes()
+            // Use the geometry data as needed
+            console.log("Selected Zone Geometry: ", geometry);
+            console.log("SELECTED ZONE COORDS: ", selectedZoneCoords)
+          }
+
 
         console.log("Selected Zones: ", Object.keys(selectedZones));
-        map.data.revertStyle();
         map.data.setStyle((feature) => {
-          const featureZoneId =
-            feature.getProperty("boro_cd") ||
-            feature.getProperty("boro_code") ||
-            feature.getProperty("cong_dist") ||
-            feature.getProperty("elect_dist") ||
-            feature.getProperty("precinct") ||
-            feature.getProperty("school_dist");
-          const isSelected = !!selectedZones[featureZoneId];
-
+          const featureZoneId = getZoneId(feature);
+          const isSelected = selectedZones[featureZoneId];
+        
           return {
             fillColor: isSelected ? "#00FF00" : "#FF3333",
             strokeColor: isSelected ? "#00FF00" : "#FF5555",
             strokeWeight: 1,
           };
         });
-
-      });
-
-      // on mouseover, show the borough and zoneID
-      map.data.addListener('mouseover', (event)=>{
-
-      });
-
+      })
     })
     .catch((err) => {
       console.error("Error loading GeoJSON data", err);
     });
 }
 
+// Load the Google Maps API script with the API key from the express get('/api/google-maps-key') call
+// This is using the legacy code which relies on the api call rather than the NPM package: @googlemaps/js-api-loader
 fetch("http://localhost:3000/api/google-maps-key")
   .then((response) => response.json())
   .then((data) => {
     const scriptId = "google-maps-api-script";
     if (!document.getElementById(scriptId)) {
+      //for google maps api script
       const script = document.createElement("script");
       script.id = scriptId;
-      // Use &libraries=places to include the places api 
-      script.src = `https://maps.googleapis.com/maps/api/js?key=${data.googleMapsApiKey}&callback=initMap`;
+      // Use &libraries=places to include the places api
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${data.googleMapsApiKey}&libraries=places,geometry&callback=initMap`;
       script.async = true;
+
+      //for turf script
+      const turfy = document.createElement('script')
+      turfy.id = "turf_import"
+      turfy.src = 'https://unpkg.com/@turf/turf'
+      turfy.async = true;
+
       document.body.appendChild(script);
+      document.body.appendChild(turfy);
     }
   });
 
+// At the end of your JavaScript file
+document.addEventListener("DOMContentLoaded", (event) => {
+  const searchForm = document.getElementById("searchForm");
+  const applyFiltersButton = document.getElementById("applyFiltersButton");
+  const ratingFilterSelect = document.getElementById("ratingFilter");
+
+  const point = turf.point([-74.0060, 40.7128]); // New York City
+  const buffered = turf.buffer(point, 1, { units: 'miles' });
+  console.log("Buffered: ", buffered);
+
+  searchForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    searchForm.reset();
+    // Add logic for what should happen when the form is submitted
+  });
+
+  ratingFilterSelect.addEventListener("change", (event) => {
+    // You can directly call applyFilters here if that's the intended behavior
+    event.preventDefault();
+    // applyFilters();
+  });
+
+  applyFiltersButton.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // applyFilters();
+  });
+});
