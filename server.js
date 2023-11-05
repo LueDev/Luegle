d223b5c0dff9cfe73d4f9b4e9a07f2f1635a3879
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-11-04 23:26:49 -0400

    I think whenever git is fast forwarded, it will try to remove any changes between the local and the fast forward to resolve merging issues. Meaning it will remove any work between the last commit to the new/current commit to resolve the issue. Re-added the get /api/places route in server.js

--- server.js
+++ server.js
@@ -26,8 +26,25 @@ app.get("/api/google-maps-key", (req, res) => {
   res.json({ googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY });
 });
 
-app.use('/scripts', express.static(path.join(__dirname, 'public')))
-app.use('/app.js', express.static('app.js')); // Serve app.js statically
+app.get("/api/places", async (req, res) => {
+  const { center, radius } = req.query;
+
+  try {
+    const response = await fetch(
+      `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${center}&radius=${radius}&type=place_type&keyword=restaurant&key=${process.env.GOOGLE_MAPS_API_KEY}`
+    );
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error("ERROR WITH THE PLACES API fetch: ", error);
+    res
+      .status(500)
+      .json({ error: "Error fetching data from Google Places API" });
+  }
+});
+
+app.use("/scripts", express.static(path.join(__dirname, "public")));
+app.use("/app.js", express.static("app.js")); // Serve app.js statically
 
 //importing the geojson data statically for express js to utilize:
 app.use(
