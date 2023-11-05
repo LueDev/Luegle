335c2e106600193037ddc8b3a6ee1cc165686722
LueDev <41023873+LueDev@users.noreply.github.com>
2023-11-04 23:12:33 -0400

    Update server.js
    Added the route get '/api/places' using the request.query to destruct the center and radius objects which uses this to make its own call from server.js to avoid CORS interference with fetching that would be seen as cross-origin fetching if sent from the browser (app.js or index.html)

--- server.js
+++ server.js
@@ -1,32 +1,55 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const fs = require('fs');
+const express = require("express");
+const dotenv = require("dotenv");
+const fs = require("fs");
 const app = express();
-const path = require('path')
+const path = require("path");
 
 dotenv.config();
 
-app.get('/', (req, res) => {
-    fs.readFile('index.html', 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-            return;
-        }
-        
-        const renderedHtml = data.replace('YOUR_API_KEY', process.env.GOOGLE_MAPS_API_KEY);
-        res.send(renderedHtml);
-    });
+app.get("/", (req, res) => {
+  fs.readFile("index.html", "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+
+    const renderedHtml = data.replace(
+      "YOUR_API_KEY",
+      process.env.GOOGLE_MAPS_API_KEY
+    );
+    res.send(renderedHtml);
+  });
 });
 
-app.get('/api/google-maps-key', (req, res) => {
-    res.json({googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY });
+app.get("/api/google-maps-key", (req, res) => {
+  res.json({ googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY });
 });
 
-app.use('/scripts', express.static(path.join(__dirname, 'public')))
-app.use('/app.js', express.static('app.js')); // Serve app.js statically
+app.get('/api/places', async (req, res) => {
+    const { center, radius } = req.query;
+    
+    try {
+      const response = await fetch(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${center}&radius=${radius}&type=place_type&keyword=restaurant&key=${process.env.GOOGLE_MAPS_API_KEY }`);
+      const data = await response.json();
+      res.json(data);
+    } catch (error) {
+      console.error("ERROR WITH THE PLACES API fetch: ", error);
+      res.status(500).json({ error: "Error fetching data from Google Places API" });
+    }
+  });
+  
 
-//importing the geojson data statically for express js to utilize: 
-app.use('/geojson', express.static('/Users/luisjorge/code/Flatiron-Phase-1/20-Phase_1_Project/Luegle/NYC_GeoJSON_Data'));
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.use("/scripts", express.static(path.join(__dirname, "public")));
+app.use("/app.js", express.static("app.js")); // Serve app.js statically
+
+//importing the geojson data statically for express js to utilize:
+app.use(
+  "/geojson",
+  express.static(
+    "/Users/luisjorge/code/Flatiron-Phase-1/20-Phase_1_Project/Luegle/NYC_GeoJSON_Data"
+  )
+);
+
+app.listen(3000, () => console.log("Server started on port 3000"));
